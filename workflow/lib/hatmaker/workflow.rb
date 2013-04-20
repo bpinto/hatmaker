e2c22de718e157348b271aa4c59ba3afe17bbddf
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-04-20 02:03:30 -0300

    Installing a workflow

---
+++ workflow/lib/hatmaker/workflow.rb
@@ -0,0 +1,22 @@
+class Hatmaker::Workflow
+  attr_reader :author, :description, :download_link, :name, :version
+
+  def initialize(params)
+    @author        = params['author']
+    @description   = params['description']
+    @download_link = params['download_link']
+    @name          = params['name']
+    @version       = params['version']
+  end
+
+  def self.search(query)
+    @workflows ||= self.all
+    @workflows.select { |w| w.name =~ /#{query}/i }
+  end
+
+  private
+
+  def self.all
+    AlfredWorkflow.all
+  end
+end
--- workflow/main.rb
+++ workflow/main.rb
@@ -2,11 +2,63 @@
 # encoding: utf-8
 
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require "bundle/bundler/setup"
-require "alfred"
+require 'bundle/bundler/setup'
+require 'alfred'
+require 'oj'
+require 'open-uri'
+
+require File.join(File.dirname(__FILE__), 'lib/alfred_workflow')
+require File.join(File.dirname(__FILE__), 'lib/hatmaker')
+require File.join(File.dirname(__FILE__), 'lib/hatmaker/workflow')
+
+def search(query, feedback)
+  workflows = Hatmaker::Workflow.search(query)
+
+  if workflows.count > 0
+    workflows.each do |workflow|
+      feedback.add_item({
+        :uid      => "#{workflow.author}_#{workflow.name}",
+        :title    => workflow.name,
+        :subtitle => "v#{workflow.version} by #{workflow.author}",
+        :arg      => Oj.dump(workflow)
+      })
+    end
+  else
+    feedback.add_item({
+      :uid      => 'nothingfound',
+      :title    => 'Workflows',
+      :subtitle => 'No workflows found with this query',
+      :valid    => 'no'
+    })
+  end
+end
+
+def install(json, feedback)
+  workflow = Oj.load(json)
+
+  File.open('/tmp/workflow.alfredworkflow', 'wb') do |saved_file|
+    open(workflow.download_link, 'rb') do |read_file|
+      saved_file.write(read_file.read)
+    end
+  end
+
+  `open /tmp/workflow.alfredworkflow`
+end
 
 Alfred.with_friendly_error do |alfred|
-  fb = alfred.feedback
-  puts fb.to_xml(ARGV)
+  alfred.with_rescue_feedback = true
+  feedback = alfred.feedback
+
+  command   = ARGV[0]
+  arguments = ARGV[1]
+
+  case command
+  when /search/
+    search arguments, feedback
+  when /install/
+    install arguments, feedback
+  end
+
+  puts feedback.to_xml
 end
 
