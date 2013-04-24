9b69aae237ab702eccf52f8868edf25ba9f66ec5
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-04-24 15:32:34 -0300

    Fixes workflows path #6

--- workflow/lib/hatmaker/alfred.rb
+++ workflow/lib/hatmaker/alfred.rb
@@ -1,6 +1,6 @@
 class Hatmaker::Alfred
   STORAGE_PATH   = "#{ENV['HOME']}/Library/Application Support/Alfred 2/Workflow Data"
-  WORKFLOWS_PATH = File.join(File.dirname(__FILE__), '../../../..')
+  WORKFLOWS_PATH = File.join(File.dirname(__FILE__), '../../..')
 
   def self.info(msg)
     logger.info msg.to_s
